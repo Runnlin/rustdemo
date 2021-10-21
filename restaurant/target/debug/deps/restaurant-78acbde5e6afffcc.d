ab41cda9dc4961ba96f1b31ca78a4980bb1ff312
Runnlin <z77072716565@gmail.com>
2021-10-21 21:46:06 +0800

    new lib restaurant

---
+++ restaurant/target/debug/deps/restaurant-78acbde5e6afffcc.d
@@ -0,0 +1,5 @@
+/Users/runnlin/Projects/rustdemo/restaurant/target/debug/deps/restaurant-78acbde5e6afffcc.rmeta: src/lib.rs
+
+/Users/runnlin/Projects/rustdemo/restaurant/target/debug/deps/restaurant-78acbde5e6afffcc.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/debug/deps/restaurant-a085fb36e2c35583.d
@@ -0,0 +1,5 @@
+/Users/runnlin/Projects/rustdemo/restaurant/target/debug/deps/restaurant-a085fb36e2c35583.rmeta: src/lib.rs
+
+/Users/runnlin/Projects/rustdemo/restaurant/target/debug/deps/restaurant-a085fb36e2c35583.d: src/lib.rs
+
+src/lib.rs:
